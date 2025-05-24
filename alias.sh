6838ea9a2c93349f260afc1c94d4be8966fe52f9
nagata <yuya.yuekaevae@gmail.com>
2025-05-25 01:11:37 +0900

    feat: 環境変数を追加した

--- alias.sh
+++ alias.sh
@@ -2,3 +2,6 @@ alias nvim="flatpak run io.neovim.nvim"
 alias copy="xclip -selection clipboard"
 alias paste="xclip -selection clipboard -o"
 
+export SHELL=/bin/bash
+export EDITOR=/bin/nvim
+export PATH="$PATH:$HOME/.local/bin"
