152a20418c4e0fdb818c8449105716ceca711ef5
nagata <yuya.yuekaevae@gmail.com>
2025-05-25 16:32:51 +0900

    feat: 仮想環境と物理環境で機能を分ける

--- alias.sh
+++ alias.sh
@@ -2,6 +2,18 @@
 alias copy="xclip -selection clipboard"
 alias paste="xclip -selection clipboard -o"
 
-if systemd-detect-virt == "none"
-    alias nvim="flatpak run io.neovim.nvim"
+# git
+alias l=" pull"
+alias h=" push"
+alias checkout="checkout develop"
+
+
+if systemd-detect-virt -q; then
+    # 仮想環境下の変数設定をここに書く
+    echo "仮想環境です"
+else
+    # 物理環境下の設定
+    echo "物理環境です"
+    alias to="toolbox enter"
 fi
+
