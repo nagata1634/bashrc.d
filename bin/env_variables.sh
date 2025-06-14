7761085425785da2c11c22a7d93562ff44cd685a
nyuuya <yuya.yuekaevae@gmail.com>
2025-06-15 01:32:52 +0900

    環境変数を追加

--- bin/env_variables.sh
+++ bin/env_variables.sh
@@ -2,3 +2,10 @@
 export SHELL=/bin/bash
 export EDITOR=/bin/nvim
 export PATH="$PATH:$HOME/.local/bin"
+
+# IME
+export INPUT_METHOD=fcitx
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS="@im=fcitx"
+
