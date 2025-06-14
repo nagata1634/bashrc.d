714eb61f0a091f52394d2f2d993c374505c227d0
nyuuya <yuya.yuekaevae@gmail.com>
2025-06-14 16:28:12 +0900

    init.sh

--- init.sh
+++ init.sh
@@ -1,30 +1,38 @@
-#!/bin/bash
-set -ueo pipefail
-shopt -s nullglob
-
+# !/bin/bash
+set -euxo pipefail
+WORKDIR=$(pwd)
 CONFIG_DIR="${OSH_CUSTOM:-$HOME/.config/bash}"
 
-cd "$CONFIG_DIR" || exit 1
+cd "$(dirname "$0")"
 
 # Git 最新化
-if [ -d .git ]; then
-    git pull origin main || {
-        echo "❌ Gitの更新に失敗しました" >&2
-        exit 1
-    }
-else
-    echo "❌ $CONFIG_DIR は Git リポジトリではありません" >&2
-fi
+function update() {
+	if test -d .git ; then
+		git pull origin main || {
+			echo "Errer: failure update"
+		}
+	else
+		echo "$CONFIG_DIR not repo"
+	fi
+
+}
 
 # bin/*.sh を読み込む（エラー処理付き）
-if [ -d "$CONFIG_DIR/bin" ]; then
-    for file in "$CONFIG_DIR"/bin/*.sh; do
-        if [ -f "$file" ]; then
-            if ! source "$file"; then
-                echo "⚠️ 読み込み失敗: $file" >&2
-            else
-                echo "✅ 読み込み成功: $file"
-            fi
-        fi
-    done
-fi
+function load_settingfile() {
+	if [ -d "$CONFIG_DIR/bin" ]; then
+	    for file in "$CONFIG_DIR"/bin/*.sh; do
+		if [ -f "$file" ]; then
+		    if ! source "$file"; then
+			echo "⚠️ 読み込み失敗: $file" >&2
+		    else
+			echo "✅ 読み込み成功: $file"
+		    fi
+		fi
+	    done
+	fi
+}
+
+update
+load_settingfile
+
+cd $WORKDIR
