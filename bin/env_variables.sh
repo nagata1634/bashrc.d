a12ebe0d40b1148fd7185ba710d092313336b5b7
Nagata1634 <yuya.yuekaevae@gmail.com>
2025-06-21 09:26:46 +0900

    add IME　ローカルバイナリ　XDGのパスを追加

--- bin/env_variables.sh
+++ bin/env_variables.sh
@@ -1,6 +1,8 @@
 # env_variables
 export SHELL=/bin/bash
 export EDITOR=/bin/nvim
+export $XDG_CONFIG_HOME=$HOME/.config
+# バイナリの場所（ローカル）
 export PATH="$PATH:$HOME/.local/bin"
 
 # IME
@@ -8,4 +10,3 @@ export INPUT_METHOD=fcitx
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 export XMODIFIERS="@im=fcitx"
-
