457002a07f81aedffccd88a47e9ff9fed211b848
Nagata1634 <yuya.yuekaevae@gmail.com>
2025-06-21 13:59:48 +0900

    retab

--- init.sh
+++ init.sh
@@ -6,25 +6,25 @@ cd "$(dirname "$0")"
 
 # Git 最新化
 function update() {
-	if test -d .git ; then
-		git pull origin main || {
-			echo "Errer: failure update"
-		}
-	fi
+    if test -d .git ; then
+        git pull origin main || {
+            echo "Errer: failure update"
+        }
+    fi
 
 }
 
 # bin/*.sh を読み込む（エラー処理付き）
 function load_settingfile() {
-	if [ -d "$CONFIG_DIR/bin" ]; then
-	    for file in "$CONFIG_DIR"/bin/*.sh; do
-		if [ -f "$file" ]; then
-		    if ! source "$file"; then
-			echo "⚠️ 読み込み失敗: $file" >&2
-		    fi
-		fi
-	    done
-	fi
+    if [ -d "$CONFIG_DIR/bin" ]; then
+        for file in "$CONFIG_DIR"/bin/*.sh; do
+        if [ -f "$file" ]; then
+            if ! source "$file"; then
+            echo "⚠️ 読み込み失敗: $file" >&2
+            fi
+        fi
+        done
+    fi
 }
 
 update
